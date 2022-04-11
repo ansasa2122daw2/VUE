f4de4747e294f3097ac954eaacf13283ce2a5692
ansasa2122daw2 <ansasa2122daw2@protonmail.com>
2022-04-11 16:59:22 +0200

    commit sudokus creados

--- Routing.js
+++ Routing.js
@@ -1,10 +1,10 @@
 const error = {
-    data: function() {
-      return {
-        url: window.location.hash
-      };
-    },
-    template: `
+	data: function () {
+		return {
+			url: window.location.hash,
+		};
+	},
+	template: `
     <div class="main">
     <div class="puntuaciones">
         <table id="table">
@@ -22,11 +22,11 @@ const error = {
             </tr>
         </table>
     </div>
-    `
-  };
-  
-  const principal = {
-    template: `
+    `,
+};
+
+const principal = {
+	template: `
     <div class="main">
     <div class="puntuaciones">
         <table id="table">
@@ -44,47 +44,340 @@ const error = {
             </tr>
         </table>
     </div>
-    `
-  };
-  const facil = {
-    template: `
+    `,
+};
+
+const facil = {
+	data: function () {
+		return {
+			sudoku: [
+				{ valor: "", disabled: false },
+				{ valor: 6, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: 1, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: 4, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: 5, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 8, disabled: true },
+				{ valor: 3, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: 5, disabled: true },
+				{ valor: 6, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 2, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 1, disabled: true },
+				{ valor: 8, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 4, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: 7, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 6, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 6, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 3, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 7, disabled: true },
+				{ valor: 3, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: 9, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: 1, disabled: true },
+				{ valor: 8, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: 4, disabled: true },
+				{ valor: 5, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 2, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 7, disabled: true },
+				{ valor: 2, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: 6, disabled: true },
+				{ valor: 9, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 4, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: 5, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: 8, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: 7, disabled: true },
+				{ valor: "", disabled: false },
+			],
+			color: "red",
+			colorVALOR: "solid black",
+		};
+	},
+	template: `
+    <div>
       <p>Facil</p>
-    `
-  };
-  const medio = {
-    template: `
+        <div class="sudoku">
+            <div class="casilla" v-for="(numero,index) in sudoku" :key="index">
+
+                <input type="number" v-model="numero.valor" :disabled="numero.disabled" v-bind:style="[numero.disabled ?{colorVAL}:{color}]" />
+            </div>
+        </div>
+      </div>
+
+    `,
+};
+const medio = {
+	data: function () {
+		return {
+			sudokuM: [
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 9, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: 4, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 5, disabled: true },
+				{ valor: 3, disabled: true },
+				{ valor: 1, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 6, disabled: true },
+				{ valor: 1, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 8, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: 5, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 5, disabled: true },
+				{ valor: 4, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 2, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: 3, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: 1, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 7, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 8, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: 8, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 7, disabled: true },
+				{ valor: 6, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: 3, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: 6, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: 1, disabled: true },
+				{ valor: 9, disabled: true },
+				{ valor: 4, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 7, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 4, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: 5, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: 6, disabled: true },
+				{ valor: 2, disabled: true },
+				{ valor: 7, disabled: true },
+			],
+			color: "red",
+			colorVALOR: "solid black",
+		};
+	},
+	template: `
+    <div>
       <p>Medio</p>
-    `
-  };
-  const dificil = {
-    template: `
+        <div class="sudoku">
+            <div class="casilla" v-for="(numero,index) in sudokuM" :key="index">
+                
+            <input type="number" v-model="numero.valor" :disabled="numero.disabled" v-bind:style="[numero.disabled ?{colorVAL}:{color}]"/>
+            </div>
+        </div>
+    </div>  
+    `,
+};
+const dificil = {
+	data: function () {
+		return {
+			sudokuD: [
+				{ valor: 8, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 3, disabled: true },
+				{ valor: 6, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 7, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 9, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: 2, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 5, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 7, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 4, disabled: true },
+				{ valor: 5, disabled: true },
+				{ valor: 7, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 1, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 3, disabled: true },
+				{ valor: 4, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 1, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 6, disabled: true },
+				{ valor: 8, disabled: true },
+				{ valor: 4, disabled: true },
+				{ valor: 3, disabled: true },
+				{ valor: 8, disabled: true },
+				{ valor: 5, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 1, disabled: true },
+				{ valor: 7, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: 9, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+				{ valor: 4, disabled: true },
+				{ valor: "", disabled: false },
+				{ valor: "", disabled: false },
+			],
+			color: "red",
+			colorVALOR: "solid black",
+		};
+	},
+	template: `
+    <div>
       <p>Díficil</p>
-    `
-  };  
-  const rutes = {
-    '#/': principal,
-    '#/facil': facil,
-    '#/medio': medio,
-    '#/dificil': dificil
-  };
-  
-  var routing = new Vue({
-    el: '#routing',
-    data: {
-      rutaActual: window.location.hash,
-      rutes: rutes
-    },
-    methods: {
-      navegar: function($event) {
-        this.rutaActual = $event.target.hash;
-      }
-    },
-    computed: {
-      vistaActual: function() {
-        return this.rutes[this.rutaActual] || error;
-      }
-    },
-    template: `
+        <div class="sudoku">
+            <div class="casilla" v-for="(numero,index) in sudokuD" :key="index">
+                
+            <input type="number" v-model="numero.valor" :disabled="numero.disabled" v-bind:style="[numero.disabled ?{colorVAL}:{color}]" />
+            </div>
+        </div>
+    </div>  
+    `,
+};
+const rutes = {
+	"#/": principal,
+	"#/facil": facil,
+	"#/medio": medio,
+	"#/dificil": dificil,
+};
+
+var routing = new Vue({
+	el: "#routing",
+	data: {
+		rutaActual: window.location.hash,
+		rutes: rutes,
+	},
+	methods: {
+		navegar: function ($event) {
+			this.rutaActual = $event.target.hash;
+		},
+	},
+	computed: {
+		vistaActual: function () {
+			return this.rutes[this.rutaActual] || error;
+		},
+	},
+	template: `
     <div>
     <div v-bind:is="vistaActual">        
       </div>
@@ -99,32 +392,34 @@ const error = {
     </div>
     
       </div>
-    `
-  });
+    `,
+});
 
-  /*
-  FÁCIL
-  
-  let sudoku = [9,6,3,1,7,4,2,5,8,
-                1,7,8,3,2,5,6,4,9,
-                2,5,4,6,8,9,7,3,1,
-                8,2,1,4,3,7,5,9,6,
-                4,9,6,8,5,2,3,1,7,
-                7,3,5,9,6,1,8,5,4,
-                5,8,9,7,1,3,4,6,2,
-                3,1,7,2,4,6,9,8,5,
-                6,4,2,5,9,8,1,7,3];
+/*FÁCIL;
 
-  let sudokuResolver = [0,6,0,1,0,4,0,5,0,
-                        0,0,8,3,0,5,6,0,0,
-                        2,0,0,0,0,0,0,0,1,
-                        8,0,0,4,0,7,0,0,6,
-                        0,0,6,0,0,0,3,0,0,
-                        7,0,0,0,0,0,0,0,2,
-                        0,0,7,2,0,6,9,0,0,
-                        0,4,0,5,0,8,0,7,0];
-*/
+let sudoku = [
+	9, 6, 3, 1, 7, 4, 2, 5, 8,
+	1, 7, 8, 3, 2, 5, 6, 4, 9,
+	2, 5, 4, 6, 8, 9, 7, 3, 1,
+	8, 2, 1, 4, 3, 7, 5, 9, 6,
+	4, 9, 6, 8, 5, 2, 3, 1, 7,
+	7, 3, 5, 9, 6, 1, 8, 5, 4,
+	5, 8, 9, 7, 1, 3, 4, 6, 2,
+	3, 1, 7, 2, 4, 6, 9, 8, 5,
+	6, 4, 2, 5, 9, 8, 1, 7, 3,
+];
 
+let sudokuResolver = [
+	0, 6, 0, 1, 0, 4, 0, 5, 0,
+	0, 0, 8, 3, 0, 5, 6, 0, 0,
+	2, 0, 0, 0, 0, 0, 0, 0, 1,
+	8, 0, 0, 4, 0, 7, 0, 0, 6,
+	0, 0, 6, 0, 0, 0, 3, 0, 0,
+    7, 3, 0, 9, 0, 1, 8, 0, 4,
+	5, 0, 0, 0, 0, 0, 0, 0, 2,
+	0, 0, 7, 2, 0, 6, 9, 0, 0,
+	0, 4, 0, 5, 0, 8, 0, 7, 0,
+];*/
 /* MEDIO
 
   let sudoku = [5,3,9,1,4,6,8,7,2,
